257a1479f4677caa99b6a1695f7f6e94d43936d1
Jon Bretman <jon.bretman@corp.badoo.com>
2013-10-19 03:08:00 +0100

    add logging for server starting

--- app.js
+++ app.js
@@ -4,3 +4,5 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.listen(3000);
+console.log('Server started at http://localhost:3000');
+console.log('Ready for Film Hangman!');
