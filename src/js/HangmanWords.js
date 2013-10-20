df6a7b8415bb25d39e45f8a24fece8b8cbb0438e
Jon Bretman <jon.bretman@corp.badoo.com>
2013-10-20 10:21:01 +0100

    add guessing functionality and keep track of how many letters are left to guess

--- src/js/HangmanWords.js
+++ src/js/HangmanWords.js
@@ -1,18 +1,23 @@
-define('HangmanPhrase', function () {
+define('HangmanWords', ['DOM', 'Events'], function ($, Events) {
 
-    var HangmanPhrase = function (phrase) {
+    var HangmanWords = function (words) {
         _.bindAll.apply(_, [this].concat(_.functions(this)));
 
-
-
         this.createElement_();
+
+        if (words) {
+            this.setWords(words);
+        }
+
     };
 
-    HangmanPhrase.prototype = {
+    HangmanWords.prototype = _.extend(HangmanWords.prototype, Events, {
 
         el: null,
 
-        phrase: null,
+        words_: null,
+
+        leftToGuess_: 0,
 
         appendTo: function (el) {
             el.appendChild(this.el);
@@ -20,34 +25,80 @@ define('HangmanPhrase', function () {
         },
 
         createElement_: function () {
-
             this.el = document.createElement('div');
-            this.el.className = 'hangman-phrase';
+            this.el.className = 'hangman-words';
+            return this;
+        },
 
-            _.each(this.phrase, function (word) {
+        renderWords_: function () {
+
+            var letters = [];
+            var unique = this.leftToGuess_ = [];
+
+            _.each(this.words_, function (word) {
 
                 var container = document.createElement('span');
                 container.className = 'hangman-word';
 
                 _.each(word, function (letter) {
+                    letter = letter.toUpperCase();
                     var d = document.createElement('span');
                     d.innerText = letter;
-                    d.className = 'hangman-letter';
+                    d.className = 'hangman-letter hidden hangman-letter-' + letter;
                     container.appendChild(d);
+                    letters.push($(d));
+
+                    if (unique.indexOf(letter) === -1) {
+                        unique.push(letter);
+                    }
+
                 });
 
                 this.el.appendChild(container);
 
             }, this);
+
+            var showNextLetter = _.bind(function () {
+                var letter = letters.shift();
+                if (!letter) {
+                    this.trigger('ready');
+                }
+                else {
+                    letter.removeClass('hidden');
+                    setTimeout(showNextLetter, 150);
+                }
+            }, this);
+
+            showNextLetter();
+
+            return this;
         },
 
-        setPhrase: function (phrase) {
-            this.phrase = _.map(phrase.split(' '), function (word) {
+        setWords: function (words) {
+
+            this.words_ = _.map(words.split(' '), function (word) {
                 return word.split('');
             });
+
+            this.renderWords_();
+            return this;
+        },
+
+        makeGuess: function (guess) {
+
+            if (this.leftToGuess_.indexOf(guess) === -1) {
+                return 0;
+            }
+
+            this.leftToGuess_ = _.without(this.leftToGuess_, guess);
+            return $(this.el).find('.hangman-letter-' + guess).addClass('guessed').length;
+        },
+
+        getLeftToGuess: function () {
+            return this.leftToGuess_.length;
         }
 
-    };
+    });
 
-    return HangmanPhrase;
+    return HangmanWords;
 });
